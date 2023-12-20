b0eeeff228dbfc36279f372f521b0b6419b63bbe
Aaron Kable <aaronkable@gmail.com>
2023-12-20 10:57:00 +0800

    Add some basic fleet tools to the API

--- aacat/api.py
+++ aacat/api.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import Group, User
 from django.db.models import (CharField, Count, ExpressionWrapper, F,
                               IntegerField, Max, Value)
 from django.utils import timezone
+from esi.models import Token
 from ninja import NinjaAPI
 from ninja.security import django_auth
 
@@ -83,11 +84,14 @@ def post_character_search(request, search_text: str, limit: int = 10):
 
 
 @api.post(
-    "/fleets/track/me",
+    "/fleets/track",
     response={200: List[schema.Character], 403: str},
-    tags=["Actions"]
+    tags=["Tracking"]
 )
 def post_track_me(request):
+    """
+        Track any fleeets currently being run by the logged in user.
+    """
     if not request.user.has_perm('aacat.create_fleets'):
         return 403, "No Perms"
     char_ownerships = request.user.character_ownerships.all()
@@ -101,11 +105,14 @@ def post_track_me(request):
 
 
 @api.post(
-    "/fleets/track/{character_id}",
+    "/fleets/{character_id}/track",
     response={200: schema.Character, 403: str},
-    tags=["Actions"]
+    tags=["Tracking"]
 )
 def post_track_character(request, character_id: int):
+    """
+        Track the fleet being run by the character_id, if they are in a fleet.
+    """
     if not request.user.has_perm('aacat.create_fleets'):
         return 403, "No Perms"
     character = EveCharacter.objects.get(character_id=character_id)
@@ -115,11 +122,14 @@ def post_track_character(request, character_id: int):
 
 
 @api.post(
-    "/fleets/end/{fleet_id}",
+    "/fleets/{fleet_id}/end",
     response={200: list, 403: str},
-    tags=["Actions"]
+    tags=["Tracking"]
 )
 def post_end_fleet(request, fleet_id: int):
+    """
+        Stop Tracking a fleet.
+    """
     if not request.user.has_perm('aacat.create_fleets'):
         return 403, "No Perms"
     fleets = models.Fleet.objects.filter(eve_fleet_id=fleet_id)
@@ -132,11 +142,14 @@ def post_end_fleet(request, fleet_id: int):
 
 
 @api.post(
-    "/fleets/restart/{fleet_id}",
+    "/fleets/{fleet_id}/restart",
     response={200: list, 403: str},
-    tags=["Actions"]
+    tags=["Tracking"]
 )
 def post_restart_fleet_tasks(request, fleet_id: int):
+    """
+        Restart any fleet tasks that may have failed.
+    """
     if not request.user.has_perm('aacat.create_fleets'):
         return 403, "No Perms"
     fleets = models.Fleet.objects.filter(eve_fleet_id=fleet_id)
@@ -151,9 +164,12 @@ def post_restart_fleet_tasks(request, fleet_id: int):
 @api.get(
     "/fleets/active/",
     response={200: List},
-    tags=["Fleets"]
+    tags=["Stats"]
 )
 def get_fleets_active(request, limit: int = 50):
+    """
+        Show all actively tracked fleets
+    """
     if not request.user.has_perm('aacat.create_fleets'):
         return 403, "No Perms"
 
@@ -170,9 +186,12 @@ def get_fleets_active(request, limit: int = 50):
 @api.get(
     "/fleets/recent/",
     response={200: List},
-    tags=["Fleets"]
+    tags=["Stats"]
 )
 def get_fleets_recent(request, days_look_back: int = 14):
+    """
+        Show a list of previously tracked fleets
+    """
     if not request.user.has_perm('aacat.view_global'):
         return 403, "No Perms"
 
@@ -190,11 +209,14 @@ def get_fleets_recent(request, days_look_back: int = 14):
 
 
 @api.get(
-    "/fleets/snapshot/{fleet_id}",
+    "/fleets/{fleet_id}/snapshot",
     response={200: schema.Snapshot},
-    tags=["Fleets"]
+    tags=["Stats"]
 )
 def get_fleet_recent_snapshot(request, fleet_id: int):
+    """
+        Provide teh most recent snapshot of a fleet
+    """
     if not request.user.has_perm('aacat.view_global'):
         return 403, "No Perms"
 
@@ -230,11 +252,14 @@ def get_fleet_recent_snapshot(request, fleet_id: int):
 
 
 @api.get(
-    "/fleets/stats/{fleet_id}",
+    "/fleets/{fleet_id}/stats",
     response={200: list},
-    tags=["Fleets"]
+    tags=["Stats"]
 )
 def get_fleet_stats(request, fleet_id: int):
+    """
+        Provide the most recent snapshot of a fleet grouped by ship types etc.
+    """
     if not request.user.has_perm('aacat.view_global'):
         return 403, "No Perms"
 
@@ -250,3 +275,131 @@ def get_fleet_stats(request, fleet_id: int):
         type_id=F("ship_type_id")
     )
     return ship_counts
+
+
+@api.get(
+    "/fleets/{character_id}/fleet_id",
+    response={200: int, 404: str, 403: str},
+    tags=["Search"]
+)
+def get_fleet_id_from_character_id(request, character_id: int):
+    """
+        Get the fleet id a character is in.
+    """
+    if not request.user.has_perm('aacat.view_global'):
+        return 403, "No Perms"
+
+    token = Token.get_token(
+        character_id, ['esi-location.read_online.v1', 'esi-fleets.read_fleet.v1'])
+
+    if not token:
+        return 404, "No Tokens Found"
+
+    online_status = providers.esi.client.Location.get_characters_character_id_online(character_id=character_id,
+                                                                                     token=token.valid_access_token()).result()
+    if online_status.get('online', False):
+        return 404, f"{token.character_name} is offline"
+
+    fleet_details = providers.esi.client.Fleets.get_characters_character_id_fleet(character_id=character_id,
+                                                                                  token=token.valid_access_token()).result()
+    return 200, fleet_details.get('fleet_id')
+
+
+@api.get(
+    "/fleets/{fleet_id}/details",
+    response={200: dict, 403: str},
+    tags=["Actions"]
+)
+def get_fleet_details(request, fleet_id: int):
+    """
+        Get the fleet settigns and MOTD
+    """
+    if not request.user.has_perm('aacat.create_fleets'):
+        return 403, "No Perms"
+
+    fleet = models.Fleet.objects.get(eve_fleet_id=fleet_id)
+    token = Token.get_token(fleet.boss.character_id, [
+                            'esi-fleets.write_fleet.v1'])
+    details = providers.esi.client.Fleets.get_fleets_fleet_id(
+        fleet_id=fleet_id,
+        token=token.valid_access_token()
+    ).result()
+    return details
+
+
+@api.put(
+    "/fleets/{fleet_id}/details",
+    response={200: dict, 403: str},
+    tags=["Actions"]
+)
+def put_fleet_details(request, fleet_id: int, free_move: bool, motd: str):
+    """
+        Update fleet Free move and MOTD
+    """
+    if not request.user.has_perm('aacat.create_fleets'):
+        return 403, "No Perms"
+
+    fleet = models.Fleet.objects.get(eve_fleet_id=fleet_id)
+    token = Token.get_token(fleet.boss.character_id, [
+                            'esi-fleets.write_fleet.v1'])
+    details = providers.esi.client.Fleets.put_fleets_fleet_id(
+        fleet_id=fleet_id,
+        new_settings={
+            "is_free_move": free_move,
+            "motd": motd
+        },
+        token=token.valid_access_token()
+    ).result()
+    return {
+        "is_free_move": free_move,
+        "motd": motd
+    }
+
+
+@api.delete(
+    "/fleets/{fleet_id}/kick/{character_id}",
+    response={200: str, 403: str},
+    tags=["Actions"]
+)
+def kick_fleet_member(request, fleet_id: int, character_id: int):
+    """
+        Kick character from fleet
+    """
+    if not request.user.has_perm('aacat.create_fleets'):
+        return 403, "No Perms"
+
+    fleet = models.Fleet.objects.get(eve_fleet_id=fleet_id)
+    token = Token.get_token(fleet.boss.character_id, [
+                            'esi-fleets.write_fleet.v1'])
+    status = providers.esi.client.Fleets.delete_fleets_fleet_id_members_member_id(
+        fleet_id=fleet_id,
+        member_id=character_id,
+        token=token.valid_access_token()
+    ).result()
+    return f"Kicked {character_id} from {fleet_id}"
+
+
+@api.post(
+    "/fleets/{fleet_id}/invite/{character_id}",
+    response={200: str, 403: str},
+    tags=["Actions"]
+)
+def invite_fleet_member(request, fleet_id: int, character_id: int):
+    """
+        Invite a character to fleet
+    """
+    if not request.user.has_perm('aacat.create_fleets'):
+        return 403, "No Perms"
+
+    fleet = models.Fleet.objects.get(eve_fleet_id=fleet_id)
+    token = Token.get_token(fleet.boss.character_id, [
+                            'esi-fleets.write_fleet.v1'])
+    join = providers.esi.client.Fleets.post_fleets_fleet_id_members(
+        fleet_id=fleet_id,
+        invitation={
+            "character_id": character_id,
+            "role": "squad_member"
+        },
+        token=token.valid_access_token()
+    ).result()
+    return f"sent Invite to {character_id} from {fleet_id} aka {token.character_name}"
